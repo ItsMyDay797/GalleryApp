00337a38de20f6c538f0664ca9e6422a45b2049f
ItsMyDay797 <m.rus1298@gmail.com>
2026-02-10 12:00:00 +0300

    Initial Commit

---
+++ GalleryAppV3Tests/GalleryAppV3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppV3Tests.swift
+//  GalleryAppV3Tests
+//
+//  Created by Марк Русаков on 10.02.26.
+//
+
+import Testing
+@testable import GalleryAppV3
+
+struct GalleryAppV3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
